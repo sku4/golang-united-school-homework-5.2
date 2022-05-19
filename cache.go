783908f3f31f44a091e11124fff861c89c054260
Скубач Сергей <s.skubach@e-comexpert.com>
2022-05-19 23:11:31 +0300

    solution

--- cache.go
+++ cache.go
@@ -2,22 +2,59 @@ package cache
 
 import "time"
 
+type valueTime struct {
+	value    string
+	deadline time.Time
+}
+
 type Cache struct {
+	data map[string]*valueTime
 }
 
 func NewCache() Cache {
-	return Cache{}
+	return Cache{
+		data: make(map[string]*valueTime),
+	}
 }
 
-func (receiver) Get(key string) (string, bool) {
-
+func (c *Cache) Get(key string) (string, bool) {
+	v := ""
+	exist := false
+	for _, k := range c.Keys() {
+		if k == key {
+			exist = true
+			d := c.data[key]
+			if d != nil {
+				v = d.value
+			}
+			break
+		}
+	}
+	return v, exist
 }
 
-func (receiver) Put(key, value string) {
+func (c *Cache) Put(key, value string) {
+	c.data[key] = &valueTime{
+		value:    value,
+		deadline: time.Unix(0, 0),
+	}
 }
 
-func (receiver) Keys() []string {
+func (c *Cache) Keys() []string {
+	var s []string
+	for k, vTime := range c.data {
+		if vTime.deadline.UnixMicro() < time.Now().UnixMicro() && vTime.deadline.UnixMicro() > 0 {
+			delete(c.data, k)
+		} else {
+			s = append(s, k)
+		}
+	}
+	return s
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (c *Cache) PutTill(key, value string, deadline time.Time) {
+	c.data[key] = &valueTime{
+		value:    value,
+		deadline: deadline,
+	}
 }
